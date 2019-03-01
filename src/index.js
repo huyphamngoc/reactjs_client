bdfb484d767b2ca3559a8e9887291f8b827b048e
Huy Ngoc Pham <huypn@nal.vn>
2019-03-01 10:22:03 +0700

    new123

--- src/index.js
+++ src/index.js
@@ -4,11 +4,7 @@ import './index.css';
 import '../src/content/css/Content-homepage.css';
 import './content/js/Content-homepage';
 import * as serviceWorker from './serviceWorker';
-import ContentHompage from "../src/content/js/Content-homepage.js";
-import LayoutHeader from "./layout/js/Layout-header";
 import App from "./App";
-import {Router, Route ,BrowserRouter} from "react-router-dom";
-import News from "./News";
 
 ReactDOM.render(
     <App/>
